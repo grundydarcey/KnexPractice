33d71ad04a36755708c238b3dee6622f550c7b27
Beyonce Knowles <you@example.com>
2021-01-07 17:28:49 -0500

    begin building query

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const knex = require('knex');
 
 const knexInstance = knex({
   client: 'pg',
   connection: process.env.DB_URL
 });
+
+console.log('knex and driver installed correctly');
+
+knexInstance.from('amazong_products')
+  .select('*');
